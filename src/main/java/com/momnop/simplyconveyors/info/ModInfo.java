6126f3f82c210e092e14dcaa4d9bb84d7f61d7da
Jacob Farley <redstonedudeiscool@gmail.com>
2016-11-08 19:18:22 -0600

    Update ModInfo to the correct version.

--- src/main/java/com/momnop/simplyconveyors/info/ModInfo.java
+++ src/main/java/com/momnop/simplyconveyors/info/ModInfo.java
@@ -3,6 +3,6 @@ package com.momnop.simplyconveyors.info;
 public class ModInfo
 {
     public static final String MODID = "simplyconveyors";
-    public static final String VERSION = "1.7.10-1.0.0";
+    public static final String VERSION = "1.1.2";
     public static final String NAME = "Simply Conveyors";
 }
