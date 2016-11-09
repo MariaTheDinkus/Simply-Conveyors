fc160dd13a712bf4b44cbf7cf1c10e952103a7d4
Jacob Farley <redstonedudeiscool@gmail.com>
2016-11-08 21:09:27 -0600

    Fix crash when breaking Conveyor Ramps

--- src/main/java/com/momnop/simplyconveyors/blocks/tiles/TileEntityFastMovingStair.java
+++ src/main/java/com/momnop/simplyconveyors/blocks/tiles/TileEntityFastMovingStair.java
@@ -3,20 +3,19 @@ package com.momnop.simplyconveyors.blocks.tiles;
 import java.util.List;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 
-import com.momnop.simplyconveyors.blocks.BlockMovingSlowStairPath;
+import com.momnop.simplyconveyors.blocks.BlockMovingFastStairPath;
 
 public class TileEntityFastMovingStair extends TileEntity implements ITickable {
 	
 	@Override
     public void update()
     {
-			BlockMovingSlowStairPath block = (BlockMovingSlowStairPath) blockType;
+			BlockMovingFastStairPath block = (BlockMovingFastStairPath) blockType;
             List entities = this.worldObj.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(
             		pos.getX(), pos.getY() + 1, pos.getZ(), pos.getX() + 1, pos.getY() + 1.1F, pos.getZ() + 1));
             for (Object obj : entities)
