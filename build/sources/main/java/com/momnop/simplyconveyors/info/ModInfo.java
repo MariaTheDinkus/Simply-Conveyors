ac1e0112f380e3f2aa13c7f44035aa2701e5ec57
Jacob Farley <redstonedudeiscool@gmail.com>
2017-07-05 21:15:04 -0500

    Remove many un-needed files.

--- build/sources/main/java/com/momnop/simplyconveyors/info/ModInfo.java
+++
@@ -1,8 +0,0 @@
-package com.momnop.simplyconveyors.info;
-
-public class ModInfo
-{
-    public static final String MODID = "simplyconveyors";
-    public static final String VERSION = "1.2.0";
-    public static final String NAME = "Simply Conveyors";
-}
